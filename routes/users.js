7c06a623d97773e4b0ad327ce02e1834e9b9cc17
Udana D. Kodikara <chipseyofficial@gmail.com>
2023-08-25 18:28:36 +0530

    User

---
+++ routes/users.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { signin, signup, fetchUsers } from "../controllers/user.js";
+
+const router = express.Router();
+
+router.post("/signin", signin);
+router.post("/signup", signup);
+router.get("/all", fetchUsers);
+
+export default router;
