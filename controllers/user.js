c98df8f18e9be12cb428c501b77bbaf40d373690
gamagegmp <malithipgamage@gmail.com>
2023-08-26 15:25:45 +0530

    update and delete User

--- controllers/user.js
+++ controllers/user.js
@@ -69,3 +69,48 @@ export const fetchUsers = async (req, res) => {
     res.status(500).json({ message: "Something went wrong!" });
   }
 };
+
+export const deleteUser = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const deletedUser = await User.findOneAndDelete({ _id: id });
+
+    if (!deletedUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json({ message: "User deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
+
+export const updateUser = async (req, res) => {
+  const { id } = req.params;
+  const { email, password, name } = req.body;
+
+  try {
+    const existingUser = await User.findById(id);
+
+    if (!existingUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    existingUser.email = email || existingUser.email;
+    existingUser.name = name || existingUser.name;
+
+    if (password) {
+      const hashedPassword = await bcrypt.hash(password, 12);
+      existingUser.password = hashedPassword;
+    }
+
+    const updatedUser = await existingUser.save();
+
+    res
+      .status(200)
+      .json({ message: "User updated successfully", user: updatedUser });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
