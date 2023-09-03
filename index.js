269eac9463dab2ef0e90127f9202110cb7077b0e
Piyumini Chathurangi <91307957+Piyumini1011@users.noreply.github.com>
2023-09-03 16:10:13 +0530

    commit1

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
-import express from "express";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
-import cors from "cors";
-import dotenv from "dotenv";
-import userRoutes from "./routes/users.js";
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var flash = require('express-flash');
+var session = require('express-session');
+var mysql = require('mysql');
+var connection  = require('./lib/db');
+var productsRouter = require('./routes/products');
+var app = express();
 
-const app = express();
-dotenv.config();
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(session({ 
+    cookie: { maxAge: 60000 },
+    store: new session.MemoryStore,
+    saveUninitialized: true,
+    resave: 'true',
+    secret: 'secret'
+}))
 
-app.use("/users", userRoutes);
+app.use(flash());
+app.use('/products', productsRouter);
 
-//Database connection
-const CONNECTION_URL = process.env.CONNECTION_URL;
-const PORT = process.env.PORT;
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
-mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() =>
-    app.listen(PORT, () =>
-      console.log(`DB Connected ~Server Running on Port: ${PORT}`)
-    )
-  )
-  .catch((error) => console.log(error.message));
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
